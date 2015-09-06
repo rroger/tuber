cf7679fd27d83ca6e7749ea2968aa2d6bd6148c0
Roger Rüttimann <roger.ruettimann@gmail.com>
2015-09-06 12:57:32 +0200

    split up standup.py. create classes, refator..

--- player.py
+++ player.py
@@ -2,37 +2,52 @@
 # -*- coding: utf-8 -*-
 
 import os
-import sys
 import time
 import signal
+from abc import ABCMeta
 from subprocess import Popen
+
 
 _status = 'idle'
 
-def play(song, kill_after=0):
-    global _status
-    if _status == 'idle':
-        try:
-            _status = 'playing'
-            if sys.platform.startswith('darwin'): # On Macosx
-                player_name = 'afplay'
-            else: # On Rasparian
-                player_name = 'omxplayer'
 
-            player = Popen([player_name, song], preexec_fn=os.setsid)
+class Player(metaclass=ABCMeta):
 
-            if kill_after > 0:
-                time.sleep(30)
-                os.killpg(player.pid, signal.SIGTERM)
-            else:
-                player.wait()
-        except Exception as e:
-            print(e)
-            _status = 'idle'
-        finally:
-            _status = 'idle'
-    else:
-        print('There is already a song playing. Please try later.')
+    def play(self, song_path, kill_after=0):
+        global _status
+        if _status == 'idle':
+            try:
+                _status = 'playing'
+
+                player = Popen([self._get_external_player_name, song_path],
+                                preexec_fn=os.setsid)
+
+                if kill_after > 0:
+                    time.sleep(kill_after)
+                    os.killpg(player.pid, signal.SIGTERM)
+                else:
+                    player.wait()
+            except Exception as e:
+                print(e)
+                _status = 'idle'
+            finally:
+                _status = 'idle'
+        else:
+            print('There is already a song playing. Please try later.')
+
+    @abstractmethod
+    def _get_external_player_name(self):
+        pass
+
+
+class RaspPlayer(Player):
+    def _get_external_player_name(self):
+        return 'afplay'
+
+
+class MacPlayer(Player):
+    def _get_external_player_name(self):
+        return 'omxplayer'
 
 
 if __name__ == '__main__':
---
+++ webserver.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+import player
+import cherrypy
+
+
+class TubeServer(object):
+    def __init__(self):
+        if sys.platform.startswith('darwin'): # On Macosx
+            self._player = player.MacPlayer()
+        else: # On Rasparian
+            self._player = player.RaspPlayer()
+
+    def index(self):
+        return "Hello World!"
+    index.exposed = True
+
+if __name__ == '__main__':
+    cherrypy.config.update("server.conf")
+    cherrypy.quickstart(TubeServer())
